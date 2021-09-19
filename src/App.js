9ac1299b9754aa8c698e9c2ddad23c1285fbd635
seunghansun726 <seunghansun.726@gmail.com>
2021-09-19 14:45:24 +0900

    added comment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
-function App() {
-  return <div>Hello World</div>
+function Food({favFood}) {
+  let foodStatement = "I Like ";
+  favFood.forEach(element => {
+    console.log(element);
+    foodStatement = `${foodStatement} ${element} `;
+  }
+  );  
+  return(
+    <h2>{foodStatement}</h2>
+  );
 }
 
+function App() {
+  return(
+    <div>
+      <h1>Hello My First React Application</h1>
+      <Food favFood = {["Apple", "Watermelon", "Orange", "Grape"]}/>
+    </div>
+  );
+}
+
+//local: http://localhost:3000
+//On Network: http://192.169.35.85:3000
 export default App;
+ 
